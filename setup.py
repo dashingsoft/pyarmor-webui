e2b2e82a283b30e27260da51032c7cef36567699
Jondy Zhao <jondy.zhao@gmail.com>
2023-05-10 10:49:28 +0800

    Increase version to 2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from sys import platform
 from setuptools import setup
 
-__version__ = '1.4.1'
+__version__ = '2.0'
 
 with open('README.rst') as f:
     long_description = f.read()
@@ -63,6 +63,6 @@ setup(
         ],
     },
 
-    install_requires=['pyarmor~=7.6.0'] + (
+    install_requires=['pyarmor~=8.2.0'] + (
         ['pywin32'] if platform == 'win32' else []),
 )
